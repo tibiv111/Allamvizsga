81bc959db85525d5a24fe4405d3a91c8f8eba43e
Váncza Tibor <59438148+tibiv111@users.noreply.github.com>
2022-02-02 20:01:17 +0200

    Prototype adblocking in contentscript

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,25 @@
-// TODO: content script
+const rules: {
+  [url: string]: () => void
+} = {
+  'https://gamecopyworld.com/games/index.php': filterGameCopyWorld,
+}
+
+function filterNYTTechnology() {
+  const app = document.getElementById('site-content')
+  const wrapper = document.getElementById('top-wrapper')
+  app.removeChild(wrapper)
+}
+
+function filterGameCopyWorld() {
+  const divs = document.getElementsByTagName('div')
+  for (const div of divs) {
+    if (div.className == 'chk') {
+      div.style.display = 'none'
+    }
+  }
+}
+
+if (document.URL in rules) {
+  console.log(document.URL)
+  rules[document.URL]()
+}
